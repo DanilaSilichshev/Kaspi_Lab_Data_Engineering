177483c6ff67962ebfbc62e6fa35d1e9446b2ff5
DanilaSilichshev <60333637+DanilaSilichshev@users.noreply.github.com>
2021-11-09 04:46:43 +0600

    Update hometask_3.sql

--- hometask_3.sql
+++ hometask_3.sql
@@ -1,5 +1,5 @@
 # Task 1
-SELECT ROW_NUMBER() OVER() AS `position_row`, `member_id`, DATE_FORMAT(`activity_date`, '%Y-%m') AS `act_year_month`, 
+SELECT ROW_NUMBER() OVER(ORDER BY `member_id`) AS `position_row`, `member_id`, DATE_FORMAT(`activity_date`, '%Y-%m') AS `act_year_month`, 
 SUM(`number_of_wagers`) AS `number_of_wagers` FROM `revenue_analysis` GROUP BY  `member_id`, DATE_FORMAT(`activity_date`, '%Y-%m')
 ORDER BY `member_id`;
 
